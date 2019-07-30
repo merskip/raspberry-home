0fda7d6923082fe099a0f763806abffb3a5559b2
Piotr Merski <merskip@gmail.com>
2019-07-30 15:17:13 +0200

    Display time and date

--- controller/home_controller.py
+++ controller/home_controller.py
@@ -1,5 +1,5 @@
-from PIL import ImageFont, Image, ImageDraw, ImageColor
-
+from datetime import datetime
+from PIL import ImageFont
 from display.display import Display
 from platform.platform import Platform
 
@@ -10,23 +10,27 @@ EPD_HEIGHT = 264
 class HomeController:
 
     font = ImageFont.truetype("fonts/Ubuntu-Medium.ttf", 15)
+    titleFont = ImageFont.truetype("fonts/Ubuntu-Bold.ttf", 18)
 
     def __init__(self, platform: Platform, display: Display):
         self.platform = platform
         self.display = display
 
     def refresh(self):
-        image_black = Image.new('1', self.display.get_size(), 255)
-        image_red = Image.new('1', self.display.get_size(), 255)
-        draw_black = ImageDraw.Draw(image_black)
-        draw_red = ImageDraw.Draw(image_red)
+        (black_image, red_image) = self.display.new_image_draw()
 
         text = ""
         for sensor in self.platform.get_sensors():
-            values = ",\n ".join(list(map(lambda c: sensor.get_value_with_unit(c), sensor.get_characteristics())))
-            text += "%s: %s\n" % (sensor.name, values)
+            values = ", ".join(list(map(lambda c: sensor.get_value_with_unit(c), sensor.get_characteristics())))
+            text += "%s:\t %s\n" % (sensor.name, values)
 
-        draw_black.rectangle([(0, 0), self.display.get_size()], fill=ImageColor.colormap["black"])
-        draw_black.multiline_text((16, 16), text, font=self.font, fill=ImageColor.colormap["white"])
+        current_time = datetime.now()
+        header = current_time.strftime("%H:%M")
+        header_size = self.titleFont.getsize(header)
+        black_image.rectangle((0, 0, (header_size[0] + 16, header_size[1] + 16)), fill=0)
+        black_image.text((8, 8), header, font=self.titleFont, fill=255)
+        red_image.text((8 + header_size[0] + 16, 8), current_time.strftime("%A, %d.%m"), font=self.titleFont)
 
-        self.display.draw(image_black, image_red)
+        black_image.multiline_text((8, header_size[1] + 16 + 8), text, font=self.font)
+
+        self.display.draw()
