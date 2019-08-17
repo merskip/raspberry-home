4b19a6b45642142b640a0bb32a0965dce95d8a7f
Piotr Merski <merskip@gmail.com>
2019-08-17 09:20:02 +0200

    Implements returns DTH11 humidity and temperature

--- sensor/dth11sensor.py
+++ sensor/dth11sensor.py
@@ -20,4 +20,7 @@ class DTH11Sensor(Sensor):
 
     def get_value(self, characteristic: Characteristic) -> object:
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, self._pin)
-        return humidity
+        if characteristic == Characteristics.humidity:
+            return humidity
+        else:
+            return temperature
