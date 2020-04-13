b8582c8dcde22a4d3611c3fbc7e37ec4d7e19ea6
Piotr Merski <merskip@gmail.com>
2020-04-13 13:58:21 +0200

    Adding requests dependency

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='raspberry_home',
     version='0.1',
-    packages=find_packages(include=['raspberry_home', 'raspberry_home.*']),
-    package_data={'raspberry_home': ['assets/*', 'assets/*/**']},
-    url='https://github.com/merskip/raspberry-home',
-    license='',
+    description='Manage your home with Raspberry Pi',
     author='Piotr Merski',
     author_email='merskip@gmail.com',
-    description='Manage your home with Raspberry Pi',
-    install_requires=['Pillow', 'SQLAlchemy'],
+    license='',
+    url='https://github.com/merskip/raspberry-home',
+    packages=find_packages(include=['raspberry_home', 'raspberry_home.*']),
+    package_data={'raspberry_home': ['assets/*', 'assets/*/**']},
     zip_safe=False,
+    install_requires=[
+        'Pillow',
+        'SQLAlchemy',
+        'requests'
+    ],
     entry_points={
         'console_scripts': [
             'run-simulator = raspberry_home.main:run_simulator'
